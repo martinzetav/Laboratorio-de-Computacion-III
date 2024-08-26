2b2698920ac4e17eaeb5a7290f75e06e0363b700
Martin Zalazar <martinzetav@gmail.com>
2024-08-26 10:06:04 -0300

    se implemento el ejercicio 3 en App.jsx

--- Integrador/integrador-react/src/App.jsx
+++ Integrador/integrador-react/src/App.jsx
@@ -1,9 +1,11 @@
 import Ejercicio2 from "./components/Ejercicio2"
+import Ejercicio3 from "./components/Ejercicio3"
 
 function App() {
   return (
     <>
-      <Ejercicio2/>
+      {/* <Ejercicio2/> */}
+      <Ejercicio3/>
     </>
   )
 }
