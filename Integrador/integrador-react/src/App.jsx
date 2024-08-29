fc6e12b52ba39c3b0f3119dc5516c08b7a92d5fa
Martin Zalazar <martinzetav@gmail.com>
2024-08-29 10:07:56 -0300

    se implemento el ejercicio 7 en App.jsx

--- Integrador/integrador-react/src/App.jsx
+++ Integrador/integrador-react/src/App.jsx
@@ -2,6 +2,7 @@ import Ejercicio2 from "./components/Ejercicio2"
 import Ejercicio3 from "./components/Ejercicio3"
 import Ejercicio5 from "./components/Ejercicio5"
 import Ejercicio6 from "./components/Ejercicio6"
+import Ejercicio7 from "./components/Ejercicio7"
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* <Ejercicio2/> */}
       {/* <Ejercicio3/> */}
       {/* <Ejercicio5/> */}
-      <Ejercicio6/>
+      {/* <Ejercicio6/> */}
+      <Ejercicio7/>
     </>
   )
 }
