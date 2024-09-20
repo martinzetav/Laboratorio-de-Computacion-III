8caf60530f740008ce8f20edbf320950f2a2358c
Martin Zalazar <martinzetav@gmail.com>
2024-09-19 22:03:18 -0300

    se implementaron los ejercicios en App.jsx

--- Integrador/integrador-react/src/App.jsx
+++ Integrador/integrador-react/src/App.jsx
@@ -1,4 +1,6 @@
 import Ejercicio10 from "./components/Ejercicio10"
+import Ejercicio11 from "./components/Ejercicio11"
+import Ejercicio13 from "./components/Ejercicio13"
 import Ejercicio2 from "./components/Ejercicio2"
 import Ejercicio3 from "./components/Ejercicio3"
 import Ejercicio5 from "./components/Ejercicio5"
@@ -15,7 +17,9 @@ function App() {
       {/* <Ejercicio6/> */}
       {/* <Ejercicio7/> */}
       {/* <Ejercicio8/> */}
-      <Ejercicio10/>
+      {/* <Ejercicio10/> */}
+      {/* <Ejercicio11/> */}
+      <Ejercicio13/>
     </>
   )
 }
